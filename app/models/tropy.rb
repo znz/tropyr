a90d17a5e558acfc0395ae8f37d4cf3bc8a44e21
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-01-10 06:15:59 +0900

    limit content size

--- app/models/tropy.rb
+++ app/models/tropy.rb
@@ -1,5 +1,6 @@
 class Tropy < ActiveRecord::Base
   validates_uniqueness_of :pageid
+  validates_length_of :content, :in => 1..500
 
   def before_validation_on_create
     self.pageid ||= ActiveSupport::SecureRandom.hex
